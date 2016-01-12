db5ebd79eba0301cadffd1f5c611fe675a8a58af
Tom Salfield <tom@open.coop>
2016-01-12 16:00:48 +0800

    YAML loader plugin works, and can have 'overrides' directory/files

--- yaml.js
+++ yaml.js
@@ -4,6 +4,30 @@
 
 var jsyaml = require('js-yaml');
 
-exports.translate = function(load) {
-  return 'module.exports = ' + jsyaml.load(load.source);
+var extend = function (a, b) {
+    for (x in b) {
+	if (Object.prototype.toString.call(a[x]) === '[object Object]') {
+	    extend(a[x], b[x]);
+	} else {
+	    a[x] = b[x];
+	}
+    }
+}
+
+exports.translate = function (load) {
+    // add overridesDir to System.map by changing config.js to include something like:
+    // System.config({
+    //   map: {
+    //     overridesDir: 'sites/siteName'
+    //   }
+    // });
+    var js = jsyaml.load(load.source);
+    if (js.overrides) {
+	return System.import("overridesDir/" + js.overrides)
+	    .then(function(m) {
+		extend(js, m)
+		return 'module.exports = ' + JSON.stringify(js);
+	    });
+    }
+    return 'module.exports = ' + JSON.stringify(js);
 }
