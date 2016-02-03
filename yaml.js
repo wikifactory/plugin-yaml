d082820064c8a209f879ef66c55fc10c9e90b898
Tom Salfield <tom@open.coop>
2016-02-03 17:22:26 +0800

    Make it work with the loader and hot-reloading

--- yaml.js
+++ yaml.js
@@ -14,6 +14,7 @@ var extend = function (a, b) {
     }
 }
 
+
 exports.translate = function (load) {
     // add overridesDir to System.map by changing config.js using something like:
     // builder.config({
@@ -21,17 +22,48 @@ exports.translate = function (load) {
     //     overridesDir: 'sites/siteName'
     //   }
     // });
+    var getOverride = function(url) {
+	return new Promise(function (resolve, reject) {
+	    var xhttp = new XMLHttpRequest();
+	    xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4) {
+		    if (xhttp.status == 200) {
+			resolve(xhttp.responseText);
+		    } else {
+			reject("No override found");
+		    }
+		}
+	    };
+	    xhttp.open("GET", url, true);
+	    xhttp.send();
+	});
+    };
+
     var js = jsyaml.load(load.source);
     if (js.overrides) {
-	return System.import("overridesDir/" + js.overrides)
-	    .then(function(m) {
-		extend(js, m);
-		return 'module.exports = ' + JSON.stringify(js);
-	    })
-	    .catch(function(e) {
-		console.log("No override found for " + js.overrides);
-		return 'module.exports = ' + JSON.stringify(js);
-	    });
+	if (!System.builder) {
+	    // as hot-reloading tries to re-import the failed System import directly
+	    // so we need to use an xmlhttprequest here instead of System.import.
+	    return getOverride(System.map.overridesDir + '/' + js.overrides.slice(0, -1))
+		.then(function(m) {
+		    extend(js, jsyaml.load(m));
+		    return 'module.exports = ' + JSON.stringify(js);
+		})
+		.catch(function(e) {
+		    console.log("No override found for " + js.overrides);
+		    return 'module.exports = ' + JSON.stringify(js);
+		});
+	} else {
+	    return System.import("overridesDir/" + js.overrides)
+		.then(function(m) {
+		    extend(js, m);
+		    return 'module.exports = ' + JSON.stringify(js);
+		})
+		.catch(function(e) {
+		    console.log("No override found for " + js.overrides);
+		    return 'module.exports = ' + JSON.stringify(js);
+		});
+        }
     }
     return 'module.exports = ' + JSON.stringify(js);
 }
