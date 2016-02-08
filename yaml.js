b7a9e8e28e4f3c5acb986688f67296044c0e0ab9
Tom Salfield <tom@open.coop>
2016-02-08 18:40:27 +0800

    overrides are imported from the yaml config module, to allow reloading

--- yaml.js
+++ yaml.js
@@ -4,16 +4,23 @@
 
 var jsyaml = require('js-yaml');
 
-var extend = function (a, b) {
-    for (x in b) {
+var extend = `var extend = function (a, b) {
+    for (var x in b) {
 	if (Object.prototype.toString.call(a[x]) === '[object Object]') {
 	    extend(a[x], b[x]);
 	} else {
 	    a[x] = b[x];
 	}
     }
-}
+}`
 
+exports.fetch = function(load, fetch) {
+      return fetch(load).catch(function (e) {
+        // if overrides file doesn't exist - fake it
+        // auto-create the file here?
+        return ""
+      })
+}
 
 exports.translate = function (load) {
     // add overridesDir to System.map by changing config.js using something like:
@@ -22,48 +29,24 @@ exports.translate = function (load) {
     //     overridesDir: 'sites/siteName'
     //   }
     // });
-    var getOverride = function(url) {
-	return new Promise(function (resolve, reject) {
-	    var xhttp = new XMLHttpRequest();
-	    xhttp.onreadystatechange = function() {
-		if (xhttp.readyState == 4) {
-		    if (xhttp.status == 200) {
-			resolve(xhttp.responseText);
-		    } else {
-			reject("No override found");
-		    }
-		}
-	    };
-	    xhttp.open("GET", url, true);
-	    xhttp.send();
-	});
-    };
-
-    var js = jsyaml.load(load.source);
-    if (js.overrides) {
-	if (!System.builder) {
-	    // as hot-reloading tries to re-import the failed System import directly
-	    // so we need to use an xmlhttprequest here instead of System.import.
-	    return getOverride(System.map.overridesDir + '/' + js.overrides.slice(0, -1))
-		.then(function(m) {
-		    extend(js, jsyaml.load(m));
-		    return 'module.exports = ' + JSON.stringify(js);
-		})
-		.catch(function(e) {
-		    console.log("No override found for " + js.overrides);
-		    return 'module.exports = ' + JSON.stringify(js);
-		});
-	} else {
-	    return System.import("overridesDir/" + js.overrides)
-		.then(function(m) {
-		    extend(js, m);
-		    return 'module.exports = ' + JSON.stringify(js);
-		})
-		.catch(function(e) {
-		    console.log("No override found for " + js.overrides);
-		    return 'module.exports = ' + JSON.stringify(js);
-		});
-        }
+    var js;
+    if (load.source.trim() === "") {
+	     js = {}
+    } else {
+	     js = jsyaml.load(load.source);
     }
-    return 'module.exports = ' + JSON.stringify(js);
+    if (js.overrides) {
+      var importPath =  System.map.overridesDir + '/' + js.overrides;
+      if (System.builder) {
+        //importPath = System.baseURL + importPath;
+      }
+      var js = JSON.stringify(js);
+      var ModuleStr = `import {conf as overrides} from '${importPath}';
+	     ${extend};
+	     var js = ${js};
+       extend(js, overrides);
+       export var conf = js;`
+	    return ModuleStr
+    }
+    return "export var conf =" +  JSON.stringify(js);
 }
